4789175161f0460831758ae9aebc63c5880bc671
JonathanMabiala <62263731+JonathanMabiala@users.noreply.github.com>
2024-03-01 07:53:07 -0500

    implemented scrollView on Welcome Screen

--- WelcomeScreen.js
+++ WelcomeScreen.js
@@ -1,31 +1,33 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 
 const WelcomeScreen = () => {
   return (
-    <View style={{ flex: 1 }}>
-      <Text
-        style={{
-          padding: 40,
-          fontSize: 50,
-          textAlign: "center",
-          color: "white",
-        }}
-      >
-        Welcome to Little Lemon
-      </Text>
-      <Text
-        style={{
-          padding: 20,
-          fontSize: 38,
-          marginVertical: 8,
-          textAlign: "center",
-          color: "#EDEFEE",
-        }}
-      >
-        Little Lemon is a charming neighborhood bistro that serves simple food
-        and classic cocktails in a lively but casual environment. We would love
-        to hear more about your experience with us!
-      </Text>
+    <View style={{ flex: 0.82 }}>
+      <ScrollView>
+        <Text
+          style={{
+            padding: 40,
+            fontSize: 50,
+            textAlign: "center",
+            color: "white",
+          }}
+        >
+          Welcome to Little Lemon
+        </Text>
+        <Text
+          style={{
+            padding: 20,
+            fontSize: 38,
+            marginVertical: 8,
+            textAlign: "center",
+            color: "#EDEFEE",
+          }}
+        >
+          Little Lemon is a charming neighborhood bistro that serves simple food
+          and classic cocktails in a lively but casual environment. We would
+          love to hear more about your experience with us!
+        </Text>
+      </ScrollView>
     </View>
   );
 };
